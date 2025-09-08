e51f66762a4589e6231c9ef485cfc906bdbc43db
v1m4t3 <v1m4t3@gmail.com>
2025-09-08 13:02:08 +0200

    client: developed structure for train and toggle

--- client/src/components/TrainCardComponent.jsx
+++ client/src/components/TrainCardComponent.jsx
@@ -1,4 +1,4 @@
-import { Card, Button, Row, Col } from 'react-bootstrap';
+import { Card, Button, Row, Col, Popover, OverlayTrigger } from 'react-bootstrap';
 import '../styles/TrainCard.css';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
@@ -7,6 +7,7 @@ import { Train } from '../Models/TrainModels';
 import TrainAPI from '../API/TrainAPI.js';
 import { capitalizeWords } from '../utils.js';
 
+import { Fragment } from 'react';
 import SeatIcon from '../assets/icons/seatIcon.svg?react';
 
 function TrainCard({ train }) {
@@ -141,6 +142,7 @@ function TrainCard({ train }) {
               availableSeats={seatsDetails.availableSeats}
               occupiedSeats={seatsDetails.bookedSeats}
             />
+            <TrainCarRepresentation seatsDetails={seatsDetails} />
           </>
           ) : (
             <div>No seats information available.</div>
@@ -181,6 +183,110 @@ const CarSeatsInfo = ({ carSeats, totalSeats, availableSeats, occupiedSeats }) =
       <div className="ms-2 mb-3 fs-6">
         Total seats : {totalSeats}
       </div>
+    
     </div>
   );
 };
+
+
+import '../styles/Train.css';
+
+function groupSeatsByRow(seats) {
+  const rows = {};
+  seats.forEach((s) => {
+    const match = s.seatNumber.match(/^(\d+)([A-Z])$/);
+    if (!match) return;
+    const row = match[1];
+    rows[row] = rows[row] || [];
+    rows[row].push(s);
+  });
+
+  // ordina lettere
+  Object.keys(rows).forEach((r) => {
+    rows[r].sort((a, b) => a.seatNumber.localeCompare(b.seatNumber));
+  });
+
+  return rows;
+}
+function Seat({ seat }) {
+  const seatStatus = seat.isBooked ? "Busy" : "Available";
+
+  const popover = (
+    <Popover id={`popover-seat-${seat.seatId}`}>
+      <Popover.Body className="text-center">
+        <strong>{seat.seatNumber}</strong>
+        <br />
+        {seatStatus}
+      </Popover.Body>
+    </Popover>
+  );
+  
+  return (
+    <OverlayTrigger
+      placement="top"
+      overlay={popover}
+      trigger={['hover', 'focus']}
+      delay={{ show: 200, hide: 200 }}
+    >
+
+    <Button
+      variant='outline-light'
+      disabled={seat.isBooked} onClick={() => alert(`Seat ${seat.seatNumber} selected!`)}
+    >
+      <SeatIcon className={`seat-${seat.isBooked ? "red" : "green"}-icon`} >
+      </SeatIcon>
+    </Button>
+    </OverlayTrigger>
+  );
+}
+
+function TrainCar({ seatsInfo, columns }) {
+  const rows = groupSeatsByRow(seatsInfo.seats);
+
+  return (
+    <div className="train-car">
+      {Object.entries(rows).map(([rowNumber, rowSeats]) => (
+        // <div className="train-row" key={rowNumber}>
+        <div className={`train-row-${columns}`} key={rowNumber}>
+          <div className="train-row-label">{rowNumber}</div>
+
+          {/* sedili lato sinistro */}
+          {rowSeats.slice(0, Math.floor(columns / 2)).map((s) => (
+            <Seat key={s.seatId} seat={s} />
+          ))}
+
+          {/* corridoio */}
+          <div className="train-aisle" />
+
+          {/* sedili lato destro */}
+          {rowSeats.slice(Math.floor(columns / 2)).map((s) => (
+            <Seat key={s.seatId} seat={s} />
+          ))}
+
+          <div className="train-row-label">{rowNumber}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function TrainCarRepresentation({ seatsDetails }) {
+  const totalSeats = seatsDetails.totalSeats;
+  const columns = totalSeats === 45 ? 3: totalSeats > 45 ? 4 :2; // esempio di logica per decidere il numero di colonne
+  console.log("Rendering TrainCar with columns:", columns);
+
+  return (
+    <div className="page">
+      <TrainCar seatsInfo={seatsDetails} columns={columns} />
+
+      {/* <h2>2ª Classe</h2>
+      <TrainCar seatsInfo={seatsInfoSecond} columns={3} />
+
+      <h2>3ª Classe</h2>
+      <TrainCar seatsInfo={seatsInfoThird} columns={4} /> */}
+    </div>
+  );
+}
+
+
+
